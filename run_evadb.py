c7432dc3b2545b9c77bd9064c1be83b406befadd
Song, Eric <ericrsong@gmail.com>
2023-10-17 01:15:19 -0400

    add more try catches

--- run_evadb.py
+++ run_evadb.py
@@ -11,15 +11,11 @@ if __name__ == "__main__":
     consumer_key = input()
     print("Please enter your twitter API consumer secret:")
     consumer_secret = input()
-    # take tweet search query
-    print("What search query would you like to search twitter for?")
-    search_query = input()
 
     # access API
     try:
         auth = tweepy.OAuth2AppHandler(consumer_key, consumer_secret)
         api = tweepy.API(auth)
-        tweets = api.search_tweets(search_query)
     except Exception as e:
         print(e)
         print("These Twitter API credentials don't seem to work...")
@@ -54,24 +50,41 @@ if __name__ == "__main__":
                             ")")
     mysqldb.commit()
     '''
+    # take tweet search query
+    print("What search query would you like to search twitter for?")
+    search_query = input()
+    try:
+        tweets = api.search_tweets(search_query)
+    except Exception as e:
+        print(e)
+        print("Twitter search query failed without proper API credentials")
 
     # Connect to EvaDB and get a database cursor for running queries
     evacursor = evadb.connect().cursor()
 
     # create EvaDB database
-    print(evacursor.query("CREATE DATABASE IF NOT EXISTS eva_twitter_test WITH ENGINE = 'mysql', PARAMETERS = {\"user\":"
-                          + user + ", \"password\":" + password + ", \"host\":" + host + ", \"port\":" + port
-                          + ", \"database\":" + database + "};").df())
+    print(evacursor.query("CREATE DATABASE IF NOT EXISTS eva_twitter_test WITH ENGINE = 'mysql', PARAMETERS = {\"user\":\""
+                          + user + "\", \"password\":\"" + password + "\", \"host\":\"" + host + "\", \"port\":\"" + port
+                          + "\", \"database\":\"" + database + "\"};").df())
     # timestamp as text because evadb seems not to support datetime format
     # YYYY-MM-DD_HH:MM:SS is exactly 19 characters
     print(evacursor.query("CREATE TABLE IF NOT EXISTS tweets (id INTEGER UNIQUE, name TEXT(50), screenname TEXT(15), text TEXT(280), timestamp TEXT(19), rtwts INTEGER, likes INTEGER)"))
-    for tweet in tweets:
-        print(evacursor.query("INSERT INTO tweets (id, name, screenname, text, timestamp, rtwts, likes) VALUES ("
-                              +tweet.id + ", " + tweet.user.name + ", " + tweet.user.username + ", " + tweet.text
-                              + ", " + tweet.created_at.strftime("%Y-%m-%d %H:%M:%S") + ", " + tweet.retweet_count
-                              + ", " + tweet.favorite_count + ")"))
+    try:
+        for tweet in tweets:
+            print(evacursor.query("INSERT INTO tweets (id, name, screenname, text, timestamp, rtwts, likes) VALUES ("
+                                  +tweet.id + ", " + tweet.user.name + ", " + tweet.user.username + ", " + tweet.text
+                                  + ", " + tweet.created_at.strftime("%Y-%m-%d %H:%M:%S") + ", " + tweet.retweet_count
+                                  + ", " + tweet.favorite_count + ")"))
+    except Exception as e:
+        print(e)
+        print("No list of tweets to insert - did your search query fail?")
+
     # perform sentiment analysis
-    print("Performing sentiment analysis...")
-    print(evacursor.query("SELECT CHATGPT(\"Is the following tweet positive or negative? Only respond using \'positive\'"
-                          + "or \'negative\'. For example, Today is a nice day: positive; I hate Mondays: negative\","
-                            "text FROM eva_twitter_test.tweets)"))
+    try:
+        print("Performing sentiment analysis...")
+        print(evacursor.query("SELECT CHATGPT(\"Is the following tweet positive or negative? Only respond using \'positive\'"
+                              + "or \'negative\'. For example, Today is a nice day: positive; I hate Mondays: negative\","
+                                "text FROM eva_twitter_test.tweets)"))
+    except Exception as e:
+        print(e)
+        print("Sentiment analysis failed")
